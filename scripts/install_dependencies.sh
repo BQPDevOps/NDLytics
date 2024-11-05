087a32a672ece5e0545c39ea1c1e03b8d98b1930
Christopher <christopher@newdayfund.com>
2024-11-05 05:10:22 -0500

    Update scripts

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -4,11 +4,24 @@
 sudo apt-get update -y
 
 # Install Python 3 and pip
-sudo apt-get install -y python3 python3-pip
+sudo apt-get install -y python3 python3-pip python3-venv
 
 # Navigate to the application directory
 cd /home/ec2-user/NDLytics
 
+# Create a virtual environment if it doesn't exist
+if [ ! -d "venv" ]; then
+    python3 -m venv venv
+fi
+
+# Activate the virtual environment
+source venv/bin/activate
+
+# Upgrade pip within the virtual environment
+pip install --upgrade pip
+
 # Install required Python packages
-# pip3 install -r requirements.txt
-sed 's/[>=<]=.*//' requirements.txt | xargs -n 1 pip install --prefer-binary
+pip install -r requirements.txt
+
+# Deactivate the virtual environment
+deactivate
